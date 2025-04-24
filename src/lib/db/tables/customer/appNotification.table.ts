179ed1c3b283bcfcc5e08ca436b27f2e34102016
wassim <wassim28saad@gmail.com>
2025-04-24 18:46:17 +0100

    Restructure notification management by creating AppNotification and AppUserNotificationStatus tables, and updating index exports

--- src/lib/db/tables/customer/appNotification.table.ts
+++ src/lib/db/tables/customer/appNotification.table.ts
@@ -1,20 +1,18 @@
 import { boolean, json, pgTable, text, timestamp, uuid, varchar } from "drizzle-orm/pg-core";
 import { stores } from "../store";
 import { appNotificationTypeEnum } from "../tables.enum";
-import { AppUser } from "./appUser.table";
 
-// App user notifications
-export const AppUserNotification = pgTable("app_user_notification", {
+// IMPORTANT: this table changed from "App user notifications" to "App notifications"
+export const AppNotification = pgTable("app_notification", {
   id: uuid("id").primaryKey().defaultRandom(),
   storeId: uuid("store_id")
     .notNull()
     .references(() => stores.id),
-  appUserId: uuid("app_user_id").references(() => AppUser.id),
   type: appNotificationTypeEnum("type").notNull(),
   title: varchar("title", { length: 255 }).notNull(),
   content: text("content").notNull(),
   data: json("data").default({}),
-  isRead: boolean("is_read").default(false).notNull(),
   createdAt: timestamp("created_at").defaultNow().notNull(),
-  readAt: timestamp("read_at"),
+  // Remove appUserId and readAt as they'll be in the in the NEW TABLE "appUserNotificationStatus"
+  isGlobal: boolean("is_global").default(true).notNull(),// NEW: this field is used to determine if the notification is global or targeted
 });
